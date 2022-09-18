d24edd3455d57d2ec2220110786200e32553189b
gvlad <gvladovsky@gmail.com>
2022-09-18 17:16:39 +0300

    added JUnit demo

---
+++ src/test/java/com/demoqa/JUnitDemoTest.java
@@ -0,0 +1,40 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+public class JUnitDemoTest {
+
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("### @BeforeAll !");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("### @AfterAll !");
+    }
+
+    @BeforeEach
+    void beforeEachTest() {
+        // open("ya.ru");
+        System.out.println("###    @BeforeEach !");
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        System.out.println("###    @AfterEach !");
+    }
+
+    @Test
+    void simpleTestZero() {
+        System.out.println("###         @Test simpleTestZero() !");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void simpleTestOne() {
+        System.out.println("###         @Test simpleTestOne() !");
+        Assertions.assertTrue(3 > 2);
+    }
+}
