a4ea41d4c59d05a618ae000b3d79a4c6b0cc44ca
gvlad <gvladovsky@gmail.com>
2022-09-18 21:26:52 +0300

    added Text box test

--- src/test/java/com/demoqa/TextBoxTest.java
+++ src/test/java/com/demoqa/TextBoxTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,18 +21,28 @@ public class TextBoxTest {
     @Test
     void fillFormTest() {
         String name = "Irwing";
+        String lastName = "Price";
 
-        open("/text-box");
-        $("#userName").setValue(name);
+        open("/automation-practice-form");
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(lastName);
         $("#userEmail").setValue("gvladovsky@gmail.com");
-        $("#currentAddress").setValue("1111");
-        $("#permanentAddress").setValue("1111");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1111222234");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1996");
+        $(".react-datepicker__day--008:not(.react-datepicker__day--selected").click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();;
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#uploadPicture").uploadFromClasspath("image.png");
+        $("#currentAddress").setValue("2222");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
-        $("#output #name").shouldHave(text(name));
-        $("#output #email").shouldHave(text("gvladovsky@gmail.com"));
-        $("#output #currentAddress").shouldHave(text("1111"));
-        $("#output #permanentAddress").shouldHave(text("1111"));
 
     }
 }
