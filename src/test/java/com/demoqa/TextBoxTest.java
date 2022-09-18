8ffc3637de3d3365b1b65c459bcddf5dcfdd6f8e
gvlad <gvladovsky@gmail.com>
2022-09-18 23:01:59 +0300

    added Text box test and check

--- src/test/java/com/demoqa/TextBoxTest.java
+++ src/test/java/com/demoqa/TextBoxTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTest {
 
@@ -36,13 +35,24 @@ public class TextBoxTest {
         $("#hobbiesWrapper").$(byText("Reading")).click();;
         $("#subjectsInput").setValue("English").pressEnter();
         $("#uploadPicture").uploadFromClasspath("image.png");
-        $("#currentAddress").setValue("2222");
+        $("#currentAddress").setValue("address 1");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
-
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $$(".table-responsive").findBy(text("Student Name")).shouldHave(text("Irwing Price"));
+        $$(".table-responsive").findBy(text("Student Email")).shouldHave(text("gvladovsky@gmail.com"));
+        $$(".table-responsive").findBy(text("Gender")).shouldHave(text("Male"));
+        $$(".table-responsive").findBy(text("Gender")).shouldHave(text("Male"));
+        $$(".table-responsive").findBy(text("Mobile")).shouldHave(text("1111222234"));
+        $$(".table-responsive").findBy(text("Date of Birth")).shouldHave(text("08 April,1996"));
+        $$(".table-responsive").findBy(text("Subjects")).shouldHave(text("English"));
+        $$(".table-responsive").findBy(text("Hobbies")).shouldHave(text("Reading"));
+        $$(".table-responsive").findBy(text("Picture")).shouldHave(text("image.png"));
+        $$(".table-responsive").findBy(text("Address")).shouldHave(text("address 1"));
+        $$(".table-responsive").findBy(text("State and City")).shouldHave(text("NCR Delhi"));
     }
 }
